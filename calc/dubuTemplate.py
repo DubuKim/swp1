b407658358360df70ccc19f68fd75cfcb14f41df
DubuKim <3001day@gmail.com>
2020-06-21 19:51:05 +0900

    issue #4 Start version control about "dubu"(sum and product)'s code files(dubu.py, dubuTemplate.py, dubu.conf)

---
+++ calc/dubuTemplate.py
@@ -0,0 +1,13 @@
+html = b"""
+<html>
+	<body>
+		<form action="">
+			a = <input type="number" name ="a"><br><br>
+			b = <input type="number" name ="b"><br><br>
+			<input type="submit">
+		</form>
+		sum : %(sum)d<br><br>
+		product : %(product)d<br> 
+	</body>
+</html>
+"""
