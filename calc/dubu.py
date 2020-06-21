e63f88713f8b9db9946f4578fd148327d55a5f12
DubuKim <3001day@gmail.com>
2020-06-21 23:15:03 +0900

    issue #6 Add exception ValueError code at dubu.py. So it will print "Worng Input" when it recieve not int thing

--- calc/dubu.py
+++ calc/dubu.py
@@ -5,17 +5,24 @@ def application(environ, start_response):
 		d = parse_qs(environ['QUERY_STRING'])
 		a = d.get('a', [''])[0]
 		b = d.get('b', [''])[0]
-		if '' not in [a, b]:
-			a, b = int(a), int(b)
-			response_body = html % {
-				'sum' : str(a + b),
-				'product' : str(a * b)
-			}
-		else:
+		
+		if '' in [a, b]:
 			response_body = html % {
 				'sum' : "No Input",
-				'product' : "No Input" 
+				'product' : "No Input"
 			}
+		else:
+			try:
+				a, b = int(a), int(b)
+				response_body = html % {
+					'sum' : str(a + b),
+					'product' : str(a * b)
+				}
+			except ValueError:
+				response_body = html % {
+					'sum' : "Wrong Input",
+					'product' : "Wrong Input" 
+				}
 		start_response('200 OK', [
 		  ('Content-Type', 'text/html'),
 		  ('Content-Length', str(len(response_body)))
